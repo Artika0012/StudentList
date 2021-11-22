6218cf19aeb029bf8bca9e656709c9ab8caa43a1
Artika0012 <83173759+Artika0012@users.noreply.github.com>
2021-11-22 21:06:06 +0530

    Add files via upload

---
+++ backend/routes/student.routes.js
@@ -0,0 +1,44 @@
+let mongoose = require('mongoose'),
+  express = require('express'),
+  router = express.Router();
+
+// Student Model
+let studentSchema = require('../models/Student');
+
+// CREATE Student
+router.route('/create-student').post((req, res, next) => {
+  studentSchema.create(req.body, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      console.log(data)
+      res.json(data)
+    }
+  })
+});
+
+// READ Students
+router.route('/').get((req, res) => {
+  studentSchema.find((error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+// Get Single Student
+router.route('/edit-student/:id').get((req, res) => {
+  studentSchema.findById(req.params.id, (error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+
+
+module.exports = router;
